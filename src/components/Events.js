ba52357c90f9195ddf74250c75497f418b3f6ddf
Bart Dority <b0rgBart3@gmail.com>
2021-01-19 20:54:09 -0800

    working bootstrap slider

--- src/components/Events.js
+++ src/components/Events.js
@@ -11,36 +11,38 @@ function Events() {
                 <img
                 className="d-block w-100"
                 src="holder.js/800x400?text=First slide&bg=373940"
-                alt="First slide"
+                
                 />
-                <Carousel.Caption>
-                <h3>First slide label</h3>
+                <div className='mycaption'>
+                <h3>Upcoming Workshop</h3>
                 <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
+                </div>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                 className="d-block w-100"
                 src="holder.js/800x400?text=Second slide&bg=282c34"
-                alt="Second slide"
+               
                 />
 
-                <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
+         
+                <div className='mycaption'>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+               
             </Carousel.Item>
             <Carousel.Item>
                 <img
                 className="d-block w-100"
                 src="holder.js/800x400?text=Third slide&bg=20232a"
-                alt="Third slide"
+                
                 />
 
-                <Carousel.Caption>
+                <div className='mycaption'>
                 <h3>Third slide label</h3>
                 <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
+                </div>
             </Carousel.Item>
         </Carousel>
     );
