1d895fbeecff5749d370928aaa163428925b333f
Bart Dority <b0rgBart3@gmail.com>
2021-04-07 10:10:08 -0700

    working slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -5,36 +5,36 @@ import useProgress from "./useProgress";
 
 // import right from "../assets/images/right.svg";
 // import left from "../assets/images/left.svg";
+let slideObjects = [
+  {
+    title: "Upcoming Workshop",
+    caption: "Lorem ipsum datum filum. This is a short description of the workshop including some details like where and when it will take place - but not the full details.",
+    image: "moths",
+  },
+  {
+    title: "Exciting New WAF Class",
+    caption: "Lorem ipsum datum filum. This is a short description of the workshop including some details like where and when it will take place - but not the full details.",
+    image: "butterflies",
+  },
+  {
+    title: "Extra Special Event",
+    caption: "Lorem ipsum datum filum. This is a short description of the workshop including some details like where and when it will take place - but not the full details.",
+    image: "stones",
+  },
+  
+];
+
+let slideNumbers= slideObjects.map((slide, index) => {
+  return index;
+});
 
 function Slider({direction}) {
 
   let SLIDEINTERVAL = 5000;
 
-  let slideObjects = [
-    {
-      title: "Upcoming Workshop",
-      caption: "Lorem ipsum datum filum. This is a short description of the workshop including some details like where and when it will take place - but not the full details.",
-      image: "moths",
-    },
-    {
-      title: "Exciting New WAF Class",
-      caption: "Lorem ipsum datum filum. This is a short description of the workshop including some details like where and when it will take place - but not the full details.",
-      image: "butterflies",
-    },
-    {
-      title: "Extra Special Event",
-      caption: "Lorem ipsum datum filum. This is a short description of the workshop including some details like where and when it will take place - but not the full details.",
-      image: "stones",
-    },
-    
-  ];
-  // decrease the slide counter -- and cycle through
-  // (currentIndex -1 + slides.length) % slides.length
-  // increase:
-  // (currentIndex + 1) % slides.length;
 
-  //const [playing, setPlaying] = useState(true);
-  const slideCount = slideObjects.length;
+
+  let slideCount = slideObjects.length;
   const [width, setWidth] = useState(window.innerWidth);
   const [slideWidth, setSlideWidth] = useState(800);
   const [pos1, setPos1] = useState(window.innerWidth/2 - (slideWidth/2) - slideWidth);
@@ -50,25 +50,6 @@ function Slider({direction}) {
     setPos3( width/2 + (slideWidth/2));
   };
 
-  // function useMedia(query) {
-  //   let [matches, setMatches] = useState(
-  //     window.matchMedia(query).matches
-  //   )
-
-  //   // like CDupdate and CDMount
-  //   useEffect(()=> {
-  //     let media = window.matchMedia(this.props.query);
-  //     if (media.matches !== matches) {
-  //       setMatches( media.matches );
-  //     }
-  //     let listener = () => setMatches(media.matches);
-  //     media.addListener(listener);
-  //     return () =>
-  //       media.removeListener(listener);
-  //   }, [ query ]) 
-
-  //   return matches
-  // }
 
   useEffect(
     () => {
@@ -88,7 +69,7 @@ function Slider({direction}) {
       if (playing) {
         timeout = setTimeout(() => {
         setSlide((slide + 1) % slideObjects.length);
-        console.log(slide);
+
         handleResize();
       }, SLIDEINTERVAL);
     }
@@ -111,25 +92,65 @@ function Slider({direction}) {
 
   }
 
-  function frames() {
-    // let small = useMedia("(max-width: 400px)")
-    // let large = useMedia("(min-width: 800px)")
-    
-    //console.log(slideObjects);
-    // Output all the slides in a horizontal row
+  function Frames({animate, time}) {
+    let progress = useProgress(animate, 1500);
+    let distance, slidePos1, slidePos2, slidePos3;
+    let opacity1, opacity2, opacity3;
+    distance = pos3 - pos2;
+
+    if (direction === "left") {
+
+      slidePos3 = pos3 - (progress * distance);
+      slidePos2 = pos2 - (progress * distance);
+      slidePos1 = pos1 - (progress * distance);
+
+      opacity3 = 1*progress;
+      opacity2 = 1-progress;
+      opacity1 = 0;
+
+      if (slidePos3 <= pos2) {
+        slidePos3 = pos3;
+        slidePos2 = pos2;
+        slidePos1 = pos1;
+        opacity1 = 0;
+        opacity2 = 1;
+        opacity3 = 0;
+        let x = slideNumbers.pop();
+        slideNumbers.unshift(x);
+      }
+    } else {
+      slidePos3 = pos3 + (progress * distance);
+      slidePos2 = pos2 + (progress * distance);
+      slidePos1 = pos1 + (progress * distance);
+      opacity1 = progress;
+      opacity2 = 1- (progress);
+      opacity3 = 1- (progress);
+
+      if (slidePos2 >= pos3) {
+        slidePos3 = pos3;
+        slidePos2 = pos2;
+        slidePos1 = pos1;
+        opacity1 = 0;
+        opacity2 = 1;
+        opacity3 = 0;
+        let x = slideNumbers.shift();
+        slideNumbers.push(x);
+      }
+    }
+
     return (<>
 
-<div className='slide' style={{left: pos1, width: slideWidth, backgroundImage:"url(assets/events/"+slideObjects[0].image+".jpg)"}} key="0">
+<div className='slide' style={{left: slidePos1, opacity: opacity1, width: slideWidth, backgroundImage:"url(assets/events/"+slideObjects[slideNumbers[0]].image+".jpg)"}} key="0">
 
 </div>
-<div className='slide' key="1" style={{left: pos2, width: slideWidth, backgroundImage:"url(assets/events/"+slideObjects[1].image+".jpg)"}}>
+<div className='slide' key="1" style={{left: slidePos2, opacity: opacity2, width: slideWidth, backgroundImage:"url(assets/events/"+slideObjects[slideNumbers[1]].image+".jpg)"}}>
 <div className='slideInfo'>
   <div className='title'>{slideObjects[slide].title}</div>
   <div className='caption'>{slideObjects[slide].caption}</div>
   <div className='pill'>Learn More</div>
   </div>
 </div>
-<div className='slide' key="2" style={{left: pos3, width: slideWidth, backgroundImage:"url(assets/events/"+slideObjects[2].image+".jpg)"}}>
+<div className='slide' key="2" style={{left: slidePos3, opacity: opacity3, width: slideWidth, backgroundImage:"url(assets/events/"+slideObjects[slideNumbers[2]].image+".jpg)"}}>
 
 </div>
 
@@ -141,8 +162,10 @@ function Slider({direction}) {
   return (
     <>
   <div className="slideContainer group">
-      <ProgressBar key={slide + playing + 10} time={SLIDEINTERVAL} animate={playing} />
-  <div className="slides">{frames()}
+      {/* <ProgressBar key={slide + playing + 10} time={SLIDEINTERVAL} animate={playing} /> */}
+  <div className="slides">{
+    <Frames time={SLIDEINTERVAL} animate={playing} />
+  }
 
   </div>
   <div className="jumpDots">
---
+++ test.js
@@ -0,0 +1,18 @@
+
+
+
+let arr = [1,2,3];
+
+
+console.log("Original: ", arr);
+
+
+let x = arr.pop();
+
+console.log("x: ", x);
+
+arr.unshift(x);
+ 
+
+console.log("After unshifting: ", arr);
+
